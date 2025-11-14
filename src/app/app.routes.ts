736d0f730ca1488a1da0a910f30ea06383d35e4b
esavely <esavely>
2025-11-14 22:00:41 +0200

    app

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { EventsComponent } from './components/events/events.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: '/events', pathMatch: 'full' },
-  { path: 'events', component: EventsComponent }
-];
